ce1e6fd440d37386b65587125ca6703b845b1016
Jakob Gaardsted <jgaardsted@gmail.com>
2024-01-08 21:12:52 +0100

    x

--- for_pi.py
+++ for_pi.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 from sense_hat import SenseHat
+from blob5 import updateBlobs #Blob5 as Blob
+import time
+
+tick_s = 0.1
 
 def drawBlobs_PI(blobs):
   for blob in blobs:
@@ -7,4 +11,12 @@ def drawBlobs_PI(blobs):
     p = blob.pos
     #rect = pygame.Rect(p[0],p[1],5,5)
     #window.fill(color,rect)
-    sense.set_pixel(p[0], p[1], color)
+    print('color:', color)
+    SenseHat.set_pixel(p[0], p[1], color)
+
+
+while True: #for i in range(1,100):
+  blobs = updateBlobs(tick_s)  
+  drawBlobs_PI(blobs)
+  time.sleep(0.1)
+

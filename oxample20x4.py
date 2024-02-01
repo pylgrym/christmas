4fdd36458f12992b58bbaf982dd961f877c39528
Jakob Gaardsted <jgaardsted@gmail.com>
2024-02-01 21:01:40 +0100

    x

--- oxample20x4.py
+++ oxample20x4.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import busio
 import board
 import time
@@ -14,23 +16,50 @@ inputs = [S1, S2, S3, S4]
 GPIO.setmode(GPIO.BCM)
 
 
-def my_callback(channel):
-    print('Callback, channel:', channel)
+def do1():
+    print('do1')
+def do2():
+    print('do2')
+def do3():
+    print('do3')
+def do4():
+    print('do4')
+
+def waitLoop():
+    print('waitLoop starting')
+    i=0
+    while True:
+        print(i); i+=1
+        time.sleep(0.2)
+
+def my_callback(ch):
+    #print('Callback, channel:', ch)
+    if ch==S1: do1()
+    if ch==S2: do2()
+    if ch==S3: do3()
+    if ch==S4: do4()
 
 # https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
     
 # setting up buttons as inputs
+# print(GPIO.__file__)
+# print(dir(GPIO))
+# help('RPi.GPIO')
 for switch in inputs:
     GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     #    
-    GPIO.add_event_detect(switch, GPIO.RISING)
-    GPIO.add_event_callback(switch, my_callback, bouncetime=200)
+    GPIO.add_event_detect(switch, GPIO.RISING, bouncetime=200)
+    GPIO.add_event_callback(switch, my_callback) #, bouncetime=200)
+
 
 
 # set up lcd
 lcd = LCD(PCF8574(busio.I2C(board.SCL, board.SDA),0x27), num_rows=4, num_cols=20)
+lcd.print("Press a button!")
 
-column = 0
+waitLoop()
+
+
 
 def checkSwitches():
     #Check status of all four switches on the LCD board
@@ -38,7 +67,6 @@ def checkSwitches():
     val2 = not GPIO.input(S2)
     val3 = not GPIO.input(S3)
     val4 = not GPIO.input(S4)
-
     if val1 == GPIO.HIGH:
         return "S1"
     elif val2 == GPIO.HIGH:
@@ -52,6 +80,7 @@ def checkSwitches():
 
 try:
     lcd.print("Press a button!")
+    column = 0
     while(True):
         value = checkSwitches()
         # nothing is pressed
@@ -76,8 +105,6 @@ try:
             lcd.print("You pressed "+value+"!")
             column -= 3
         time.sleep(1)
-
-
 except KeyboardInterrupt:
     lcd.clear()
     GPIO.cleanup()
